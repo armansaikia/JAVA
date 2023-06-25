e2deada183389756645e9e8722c784c29ee61c53
Arman Saikia <armansaikia12@gmail.com>
2023-06-25 22:08:41 +0530

    v1

---
+++ Operators/increment.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class increment {
+
+    public static void main(String args[]){
+
+        int a = 5;
+
+      //  int result = a+3;  1 way of increment
+
+       //   a += 2;     2 way of increment
+
+      // int result = ++a; pre increment
+
+      // int result = a++; //post increment
+
+        System.out.println(a);
+    }
+    
+}
